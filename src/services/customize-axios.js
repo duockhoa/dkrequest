a3809ef788f3d3ae9b99b781ef49f4a393e5e3d4
Binh <dbcl@dkpharma.vn>
2025-08-03 15:21:11 +0000

    fix socket io

--- src/services/customize-axios.js
+++ src/services/customize-axios.js
@@ -11,7 +11,7 @@ const cookieOptions = {
 };
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL,
+    baseURL: process.env.REACT_APP_BACKEND_URL+ '/api/v1',
     timeout: 12000,
     headers: { 'X-Custom-Header': 'foobar' },
 });
